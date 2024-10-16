993961268177be1885495aaf712c41da9479bd47
Andy Smith <andyrsmith@gmail.com>
2024-10-16 16:36:10 +0100

    Destructure.

--- destructure.cpp
+++ destructure.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
-#include <string>
 #include <tuple>
 
 // using Tup = std::tuple<int, std::string>;
 
 auto return_tuple() { return std::make_tuple(21, "foo"); };
 
+struct S {
+    int x;
+    int y;
+};
+
 int main()
 {
     auto [a, b] = return_tuple();
     std::cout << " a=" << a << " b=" << b << std::endl;
+
+    S s{3, 4};
+
+    auto [c, d] = s;
+
+    std::cout << "c is " << c << " d is " << d << "\n";
 };
